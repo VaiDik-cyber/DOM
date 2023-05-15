5d269ce56166131bbe31c57696900b2eb11b7cf1
VaiDik-cyber <patelvaidik56@gmail.com>
2023-05-15 13:27:41 +0530

    .

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-function create3rdPost(callback) {
-  setTimeout(() => {
-      console.log('Post Three');
-      if (callback) {
-          callback();
-      }
-  }, 1000);
-}
+// function create3rdPost(callback) {
+//   setTimeout(() => {
+//       console.log('Post Three');
+//       if (callback) {
+//           callback();
+//       }
+//   }, 1000);
+// }
 
-function create4thPost() {
-  setTimeout(() => {
-      console.log('Post Four');
-  }, 2000);
-}
+// function create4thPost() {
+//   setTimeout(() => {
+//       console.log('Post Four');
+//   }, 2000);
+// }
 
-function create5thPost() {
-  setTimeout(() => {
-      console.log('Post Five');
-  }, 3000);
-}
+// function create5thPost() {
+//   setTimeout(() => {
+//       console.log('Post Five');
+//   }, 3000);
+// }
 
-create5thPost(create4thPost(create3rdPost()))
+// create5thPost(create4thPost(create3rdPost()))
+
+
+var x = function () {
+  this.a = 1;
+  console.log(this.a);
+  setTimeout(function () {
+    console.log(this.a);
+  });
+};
+var a = 10;
+var xx = new x();
