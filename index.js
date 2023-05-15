29c06ec439f6080d9a8cd0c1b922194f9c4ee390
VaiDik-cyber <patelvaidik56@gmail.com>
2023-05-15 15:59:31 +0530

    .

--- index.js
+++ index.js
@@ -22,12 +22,47 @@
 // create5thPost(create4thPost(create3rdPost()))
 
 
-var x = function () {
-  this.a = 1;
-  console.log(this.a);
-  setTimeout(function () {
-    console.log(this.a);
-  });
-};
-var a = 10;
-var xx = new x();
+let a=7;
+        var b=7;
+        // fun1();
+        var x = function () {
+            // console.log(window.a)     //unde
+            // console.log(window.b)
+                  //7
+            
+            console.log(this.a)       //unde
+            console.log(this.b)       //7
+            console.log(a)            //7
+            console.log(b)            //7
+            this.a=20;          
+            this.b=30;
+            var a = 5;
+            var b = 6;
+            console.log(this.a)       //20
+            console.log(this.b)       //30
+            console.log(a)            //7
+            console.log(b)            //30
+            // console.log(window.a)     //20
+            // console.log(window.b)     //30
+        }
+        x();
+
+
+
+
+
+
+
+
+
+
+        // function y(){
+        //   var name1 = "khushhal";
+        // }
+        // y();
+        // console.log(name1);
+        
+        // var obj ={
+        //   name: "bala",
+        // }
+        // console.log(obj.age);
